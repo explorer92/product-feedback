c4590ea2594cd8bb3a64abdbff07b7dac4bc9037
explorer92 <104924433+explorer92@users.noreply.github.com>
2023-07-19 06:39:40 +0000

    main project upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
+import './styles/base.css';
+import './styles/buttons.css';
+
+require('dotenv').config();
+
+const Suggestions = lazy(() => import('./pages/Suggestions'));
+const Roadmap = lazy(() => import('./pages/Roadmap'));
+const FeedbackDetail = lazy(() => import('./pages/FeedbackDetail'));
+const FeedbackAdd = lazy(() => import('./pages/FeedbackAdd'));
+const FeedbackEdit = lazy(() => import('./pages/FeedbackEdit'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Switch>
+          {/* Entry page for application - the home page */}
+          <Route path='/' exact>
+            <Suggestions />
+          </Route>
+
+          {/* Roadmap page */}
+          <Route path='/roadmap' exact>
+            <Roadmap />
+          </Route>
+
+          {/* Feedback detail page */}
+          <Route path='/feedback/detail/:feedbackID' exact>
+            <FeedbackDetail />
+          </Route>
+
+          {/* Edit feedback page */}
+          <Route path='/feedback/edit/:feedbackID' exact>
+            <FeedbackEdit />
+          </Route>
+
+          {/* Add new feedback page */}
+          <Route path='/feedback/add' exact>
+            <FeedbackAdd />
+          </Route>
+
+          {/* 404 page not found route ( any routes not matching go back to home page ) */}
+          <Route path='*'>
+            <Suggestions />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
